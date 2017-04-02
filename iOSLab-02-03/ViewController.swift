7d98d3be1fafbad9c4afd72f2aaa20968cd4b84d
Tomasz Jaeschke <jaeschke.tomasz@gmail.com>
2017-04-02 10:57:11 +0200

    Logika + prosty UI dla użytkownika

--- iOSLab-02-03/ViewController.swift
+++ iOSLab-02-03/ViewController.swift
@@ -9,17 +9,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+        case Clear = "Clear"
+    }
+    
+    var currentOperation = Operation.Empty
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        outputLbl.text = "0"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    @IBAction func numberPressed(_ sender: UIButton) {
+        runningNumber += "\(sender.currentTitle!)"
+        print(sender.currentTitle!)
+        outputLbl.text = runningNumber
     }
-
-
+    
+    
+    @IBAction func onAddPresed(_ sender: Any) {
+        processOperation(operation: .Add)
+    }
+    @IBAction func onSubPressed(_ sender: Any) {
+        processOperation(operation: .Subtract)
+    }
+    @IBAction func onDivPressed(_ sender: Any) {
+        processOperation(operation: .Divide)
+    }
+    @IBAction func onEqualPressed(_ sender: Any) {
+        processOperation(operation: currentOperation)
+    }
+    @IBAction func onMultiPressed(_ sender: Any) {
+        processOperation(operation: .Multiply)
+    }
+    @IBAction func onClearPressed(_ sender: Any) {
+        processOperation(operation: .Clear)
+        outputLbl.text = "0"
+    }
+    
+    
+    
+    func processOperation(operation: Operation) {
+        if currentOperation != Operation.Empty {
+            
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                } else if currentOperation == Operation.Clear {
+                    result = "0"
+                    runningNumber = "0"
+                    rightValStr = "0"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result
+            }
+            
+            currentOperation = operation
+        } else {
+            
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 }
 
